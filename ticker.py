6f8415a0ec3459f794bc12011d009981945a9f70
frans <frans.fuerst@gmail.com>
2017-05-20 19:41:01 +0200

    progress

--- ticker.py
+++ ticker.py
@@ -2,41 +2,52 @@
 
 import urllib
 import json
+import sys
+from pprint import pprint
 from datetime import datetime
 from urllib.request import urlopen
 
 URL = 'https://poloniex.com/public?command=returnTicker'
 
-def get_ticker() -> dict:
+
+def get_ticker() -> str:
     json_data = urlopen(URL).read().decode()
     open('{:%Y.%m.%d-%H.%M.%S}.json'.format(datetime.now()), 'w').write(json_data)
-    return json.loads(json_data)
+    return json_data
+
 
 def main():
 
     data = {}
     targets = set()
+    in_data = json.loads(open(sys.argv[1]).read() if len(sys.argv) > 1 else 
+                         get_ticker())
 
-    for d, detail in get_ticker().items():
+    for d, detail in in_data.items():
         c1, c2 = d.split('_')
+#        print(c1, c2)
         targets.add(c2)
-        print(c1, c2, detail['highestBid'])
+#        print(c1, c2, detail.keys())
         if c1 not in data:
             data[c1] = {}
         data[c1][c2] = detail
 
+    pprint(in_data['BTC_XEM'])
+
     print(data.keys())
+    for c1, detail in data.items():
+        print(c1, list(detail.keys()))
 
     if not set(data.keys()) == {'BTC', 'ETH', 'XMR', 'USDT'}:
         print(data.keys())
 
-    for cur1, exch in data.items():
-        for cur2 in data:
-            print(cur1, cur2, cur2 in exch)
-            
-
     print(targets)
     print(len(targets))
 
+    print(len(data['BTC']))
+    for e, detail in data['BTC'].items():
+        print(e, detail['last'])
+
 if __name__ == '__main__':
     main()
+
