52a74faf498112da6bb404f8a92ab1574cad6c51
frans <frans.fuerst+git@gmail.com>
2017-05-20 23:13:45 +0200

    progress

--- ticker.py
+++ ticker.py
@@ -20,8 +20,15 @@ def get_ticker() -> str:
     open('{:%Y.%m.%d-%H.%M.%S}.json'.format(datetime.now()), 'w').write(json_data)
     return json_data
 
+
 def magic(graph):
-    pass
+    for n in graph.nodes():
+        if n in BASE:
+            continue
+        print(n)
+        for c1, c2, d in graph.in_edges(n, data=True):
+            print('\t', c1, d['last'])
+        
 
 def main():
 
@@ -30,8 +37,7 @@ def main():
     in_data = json.loads(open(sys.argv[1]).read() if len(sys.argv) > 1 else 
                          get_ticker())
 
-    for d, detail in in_data.items():
-        graph.add_edge(*d.split('_'), detail)
+    graph.add_edges_from((*d.split('_'), in_data[d]) for d in sorted(in_data))
 
     graph.remove_nodes_from(
         (n for n in graph.nodes() 
@@ -39,23 +45,23 @@ def main():
 
     magic(graph)
 
-    pos = nx.spring_layout(graph, k=1.15, iterations=50)
-    pos.update({'XMR':  [0.2, 0.5],
-                'BTC':  [0.4, 0.5],
-                'ETH':  [0.6, 0.5],
-                'USDT': [0.8, 0.5]})
+    positions = nx.spring_layout(graph, k=1.15, iterations=50)
+    positions.update({'XMR':  [0.2, 0.5],
+                      'BTC':  [0.4, 0.5],
+                      'ETH':  [0.6, 0.5],
+                      'USDT': [0.8, 0.5]})
 
-    nx.draw_networkx_edge_labels(graph, pos, 
-        edge_labels=dict([((u, v), d['last']) for u, v, d in graph.edges(data=True)])
-    )
-    nx.draw(graph, pos, 
-        # edge_color=edge_colors, 
+    nx.draw_networkx_edge_labels(graph, positions, 
+        edge_labels=dict([((u, v), d['last']) 
+                            for u, v, d in graph.edges(data=True)]))
+    nx.draw(graph, positions, 
+        #edge_color={}, 
         node_color=[{'BTC': 'g',
                      'USDT':'r',
                      'ETH': 'c',
                      'XMR': 'y'}.get(n, 'w') for n in graph.nodes()], 
         node_size=1500)
-    nx.draw_networkx_labels(graph, pos, font_size=12)
+    nx.draw_networkx_labels(graph, positions, font_size=12)
     pylab.show()
 
 
