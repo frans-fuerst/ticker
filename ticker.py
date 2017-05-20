a25923324f354355318f0e8c288c012767c4c1e9
frans <frans.fuerst@gmail.com>
2017-05-20 16:35:56 +0200

    progress

--- ticker.py
+++ ticker.py
@@ -2,20 +2,41 @@
 
 import urllib
 import json
+from datetime import datetime
 from urllib.request import urlopen
 
 URL = 'https://poloniex.com/public?command=returnTicker'
 
+def get_ticker() -> dict:
+    json_data = urlopen(URL).read().decode()
+    open('{:%Y.%m.%d-%H.%M.%S}.json'.format(datetime.now()), 'w').write(json_data)
+    return json.loads(json_data)
+
 def main():
 
-    with urlopen(URL) as conn:
-        data = json.loads(conn.read().decode())
+    data = {}
+    targets = set()
 
-    for d, detail in data.items():
+    for d, detail in get_ticker().items():
         c1, c2 = d.split('_')
+        targets.add(c2)
         print(c1, c2, detail['highestBid'])
+        if c1 not in data:
+            data[c1] = {}
+        data[c1][c2] = detail
 
-    print(len(data))
+    print(data.keys())
+
+    if not set(data.keys()) == {'BTC', 'ETH', 'XMR', 'USDT'}:
+        print(data.keys())
+
+    for cur1, exch in data.items():
+        for cur2 in data:
+            print(cur1, cur2, cur2 in exch)
+            
+
+    print(targets)
+    print(len(targets))
 
 if __name__ == '__main__':
     main()
