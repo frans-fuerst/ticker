088f21ebc2b1ba07effb1d52331fdeaa7204fa8c
frans <frans.fuerst@gmail.com>
2017-06-16 16:06:14 +0200

    simple data converter

--- prototyping/elasticsearch.py
+++ prototyping/elasticsearch.py
@@ -4,6 +4,7 @@ import sys
 import os
 import argparse
 import time
+import ujson as json
 import logging as log
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import trader
@@ -18,17 +19,21 @@ def get_args() -> dict:
 
 def main():
     args = get_args()
-    th = trader.TradeHistory('BTC_ETH')
-    th.load()
+    market = 'BTC_ETH'
+    th = trader.TradeHistory(market)
+    th.load('server')
     if th.get_duration() < 10*3600:
+        print('fetch..')
         th.fetch_next(-1)
-        th.save()
+        #th.save()
     # data = th.data() #[-1000:]
     # [print(d) for d in data]
     bdata = th.rate_buckets(5 * 60)
     [print(d) for d in bdata]
     print('#%d/%.2fh => %d buckets' % (
         th.count(), th.get_duration() / 3600, len(bdata)))
+    with open('processed_' + market + '.json', 'w') as f:
+        json.dump(bdata, f)
 
 if __name__ == '__main__':
     main()
